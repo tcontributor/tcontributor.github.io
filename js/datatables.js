f892778e8a799b893ad8f7c4c745a496e4a0d56f
tcontributor <t.contributor@protonmail.com>
2018-10-25 00:42:34 +0300

    Revert "rewards table to source from json"
    This reverts commit 61a8bad2e77cf36172730e811ef3bb2cafe63307.

--- js/datatables.js
+++ js/datatables.js
@@ -1,10 +1,6 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
   $('#dataTable').DataTable({
-        "ajax": 'data/rewards.json'
-        ,"order": [[ 0, "desc" ], [ 3, "desc" ]]        
+        "order": [[ 0, "desc" ]]
     });
 });
-
-
-
