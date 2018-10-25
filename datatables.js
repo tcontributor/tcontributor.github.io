adb6c85b13e1543514dd6b9260344ec9627e3656
tcontributor <t.contributor@protonmail.com>
2018-10-25 04:10:50 +0300

    c.44.5
    deferRender try

--- datatables.js
+++ datatables.js
@@ -1,8 +1,9 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
   $('#dataTable').DataTable({
-        "ajax": 'data/rewards.json'
-        ,"order": [[ 0, "desc" ], [ 4, "desc" ]]        
+        "ajax": 'data/rewards.json',
+        "deferRender": true,
+        "order": [[ 0, "desc" ], [ 4, "desc" ]]        
     });
 });
 
