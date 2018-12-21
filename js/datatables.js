ebe77aa283b19cec868a7bb9ed3d31808896bdde
tcontributor <>
2018-12-21 10:45:03 +0300

    overdelegation info and faq update

---
+++ js/datatables.js
@@ -0,0 +1,10 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable').DataTable({
+        "ajax": 'data/rewards.json'
+        ,"order": [[ 0, "desc" ], [ 3, "desc" ]]        
+    });
+});
+
+
+
